bff0106b3eef4c39b7894d5f6e2076efc812f20f
Hosmell Warthon <hosmellwarthon@gmail.com>
2025-08-11 16:43:42 -0500

    Versión final de la lógica del JS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Lista de los nombres introducidos
-const listaNombres = [];
+let listaNombres = [];
 
 //Función para almacenar los nombres introducidos al hacer click
 function agregarAmigo() {
@@ -34,16 +34,32 @@ function mostrarNombres () {
 //Función para sortar los nombres con pseudoaleatoriedad
 }
 function sortearAmigo () {
+    //Si no hay nombres
     if (listaNombres.length === 0) {
         alert("No hay nombres para sortear");
         return;
     }
+    //Si solo hay 1 nombre
     if (listaNombres.length === 1) {
     alert("Añade más nombres para poder sortear.");
     return;
   }
+    //Se selecciona 1 nombre de forma aleatoria de acuerdo a la lista
     let indiceAleatorio = Math.floor(Math.random()*listaNombres.length);
     let nombreSorteado = listaNombres[indiceAleatorio];
     let resultado = document.getElementById("resultado");
     resultado.textContent = `El Amigo Secreto es: ${nombreSorteado}`;
+    //Se quita el nombre sorteado de la lista
+    let indice = listaNombres.indexOf(nombreSorteado);
+    if (indice !== -1) {
+        listaNombres.splice(indice, 1);
+    mostrarNombres();
+    }
+}
+
+//Función para reiniciar el juego de amigo secreto
+function reiniciarAmigo () {    
+    listaNombres = [];
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").textContent = "";    
 }
